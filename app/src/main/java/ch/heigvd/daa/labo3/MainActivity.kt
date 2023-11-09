dc629a4a62d7010dc20204796554dea95d4e3739
KevinFerati <kevin.ferati@heig-vd.ch>
2023-11-09 19:33:14 +0100

    avancement

--- app/src/main/java/ch/heigvd/daa/labo3/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa/labo3/MainActivity.kt
@@ -1,11 +1,70 @@
 package ch.heigvd.daa.labo3
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.core.view.children
+import ch.heigvd.daa.labo3.databinding.ActivityMainBinding
+import ch.heigvd.iict.and.labo2.Person
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
+    private var currentUser: Person? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initEvents()
+
     }
+
+    private fun initEvents() {
+        binding.mainBaseOccupationGroup.setOnCheckedChangeListener { _, _ ->
+            if (studentSelected()) {
+                binding.mainSpecificStudentGrouping.visibility = View.VISIBLE
+                binding.mainSpecificEmployeeGrouping.visibility = View.GONE
+            } else if(employeeSelected()) {
+                binding.mainSpecificStudentGrouping.visibility = View.GONE
+                binding.mainSpecificEmployeeGrouping.visibility = View.VISIBLE
+            }
+        }
+
+
+        binding.mainBaseBirthdateIcon.setOnClickListener {
+
+        }
+
+        binding.btnOk.setOnClickListener {
+            saveUser()
+        }
+
+        binding.btnCancel.setOnClickListener {
+            for (view in binding.root.children) {
+                if (view is EditText) {
+                    view.text.clear()
+                }
+            }
+        }
+    }
+
+    private fun saveUser() {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun studentSelected(): Boolean {
+        return binding.mainBaseOccupationGroup.checkedRadioButtonId == R.id.main_base_occupation_student
+    }
+
+    private fun employeeSelected(): Boolean {
+        return binding.mainBaseOccupationGroup.checkedRadioButtonId == R.id.main_specific_employee_grouping
+    }
+
+
 }
